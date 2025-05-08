36231982638dadf014c6bcb048f524f6c864afa7
VicenteSaldana <vicente.saldana@uc.cl>
2025-05-08 01:41:03 -0400

    FIX CORS

--- app/database.py
+++ app/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
@@ -14,5 +15,19 @@ Base = declarative_base()
 
 async def init_db():
     import app.models.user
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+
+    max_retries = 10
+    retry_delay = 2  # segundos
+
+    for attempt in range(1, max_retries + 1):
+        try:
+            async with engine.begin() as conn:
+                await conn.run_sync(Base.metadata.create_all)
+            print("✅ Conexión a la base de datos exitosa.")
+            break
+        except Exception as e:
+            print(f"⏳ Intento {attempt} de conexión fallido: {e}")
+            if attempt == max_retries:
+                print("❌ No se pudo conectar a la base de datos después de varios intentos.")
+                raise
+            await asyncio.sleep(retry_delay)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.api.routes import router
 from app.database import init_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Agregar direccion de producción o descomentar linea de abajo
+    # allow_origins=["*"],  # Descomentar para permitir todas las direcciones
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def on_startup():
