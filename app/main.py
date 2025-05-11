b2eefb83093e40639ea690e2e81e2973c70eadef
SCarcG1 <scarcamo.g@uc.cl>
2025-05-10 22:53:53 -0400

    testing more changes to see if it works on render

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Agregar direccion de producción o descomentar linea de abajo
+    # allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Agregar direccion de producción o descomentar linea de abajo
     allow_origins=["*"],  # Descomentar para permitir todas las direcciones
     allow_credentials=True,
     allow_methods=["*"],
@@ -17,5 +17,9 @@ app.add_middleware(
 @app.on_event("startup")
 async def on_startup():
     await init_db()
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI + PostgreSQL on Render"}
 
 app.include_router(router)
