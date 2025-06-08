06b88321b59bb851eab718968535d442f6a6f5cc
Sebastián Contreras <sebastian.cr820@gmail.com>
2025-06-08 16:37:55 -0400

    docs(database): add type hint for SessionLocal

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+SessionLocal: sessionmaker[AsyncSession] = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 class Base(DeclarativeBase):
     '''
