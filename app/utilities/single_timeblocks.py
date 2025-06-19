1001e9cb76df45ed9b2949a57fe3c6c61f02f836
Sebastián Contreras <sebastian.cr820@gmail.com>
2025-06-19 19:18:37 -0400

    fix: are_timeblocks_connected()

--- app/utilities/single_timeblocks.py
+++ app/utilities/single_timeblocks.py
@@ -10,11 +10,6 @@ class SingleTimeblock:
     start_time: time
     end_time: time
 
-    def __init__(self, weekly_timeblock: WeeklyTimeblock):
-        self.weekday_index = map_enum_weekday_to_int_weekday(weekly_timeblock.weekday)
-        self.start_time = weekly_timeblock.start_hour
-        self.end_time = weekly_timeblock.end_hour
-    
     def is_adjacent_to(self, other: 'SingleTimeblock'):
         if self.weekday_index != other.weekday_index:
             return False
@@ -23,43 +18,51 @@ class SingleTimeblock:
         if other.start_time <= self.start_time and self.start_time <= other.end_time:
             return True            
         return False
+    
+    @staticmethod
+    def from_weekly_timeblock(weekly_timeblock: WeeklyTimeblock):
+        return SingleTimeblock(
+            weekday_index=map_enum_weekday_to_int_weekday(weekly_timeblock.weekday),
+            start_time=weekly_timeblock.start_hour,
+            end_time=weekly_timeblock.end_hour
+        )
 
-
-def are_timeblocks_connected(
-    timeblock_1: SingleTimeblock,
-    timeblock_2: SingleTimeblock,
-    all_timeblocks: list[SingleTimeblock]
-):
-    # We'll work with sorted timeblocks:
-    all_timeblocks = sorted(all_timeblocks, key=lambda t: (t.weekday_index, t.start_time))
-    # Initialize a half matrix to store the results of the connections:
-    connected = {}
-    for i in range(len(all_timeblocks)):
-        connected[i] = {}
-        for j in range(i, len(all_timeblocks)):
-            connected[i][j] = None
-    # Base case 1: if the two timeblocks are the same, they are connected.
-    for i in range(len(all_timeblocks)):
-        connected[i][i] = True
-    # Base case 2: if the two timeblocks are adjacent, they are connected.
-    for i in range(len(all_timeblocks)):
-        for j in range(i + 1, len(all_timeblocks)):
-            if all_timeblocks[i].is_adjacent_to(all_timeblocks[j]):
-                connected[i][j] = True
-    # Recursive case: if the two timeblocks are connected through other timeblocks,
-    # they are connected. This takes advantage of the fact that the timeblocks are sorted.
-    for i in range(len(all_timeblocks)):
-        for j in range(i + 1, len(all_timeblocks)):
-            if connected[i][j] is None:
-                for k in range(i + 1, j):
-                    if connected[i][k] and connected[k][j]:
-                        connected[i][j] = True
-                        break
+    @staticmethod
+    def are_timeblocks_connected(
+        timeblock_1: 'SingleTimeblock',
+        timeblock_2: 'SingleTimeblock',
+        all_timeblocks: list['SingleTimeblock']
+    ):
+        # We'll work with sorted timeblocks:
+        all_timeblocks = sorted(all_timeblocks, key=lambda t: (t.weekday_index, t.start_time))
+        # Initialize a half matrix to store the results of the connections:
+        connected: dict[int, dict[int, bool]] = {}
+        for i in range(len(all_timeblocks)):
+            connected[i] = {}
+            for j in range(i, len(all_timeblocks)):
+                connected[i][j] = None
+        # Base case 1: if the two timeblocks are the same, they are connected.
+        for i in range(len(all_timeblocks)):
+            connected[i][i] = True
+        # Base case 2: if the two timeblocks are adjacent, they are connected.
+        for i in range(len(all_timeblocks)):
+            for j in range(i + 1, len(all_timeblocks)):
+                if all_timeblocks[i].is_adjacent_to(all_timeblocks[j]):
+                    connected[i][j] = True
+        # Recursive case: if the two timeblocks are connected through other timeblocks,
+        # they are connected. This takes advantage of the fact that the timeblocks are sorted.
+        for i in range(len(all_timeblocks)):
+            for j in range(i + 1, len(all_timeblocks)):
                 if connected[i][j] is None:
-                    connected[i][j] = False
-    # Final result:
-    timeblock_1_index = all_timeblocks.index(timeblock_1)
-    timeblock_2_index = all_timeblocks.index(timeblock_2)
-    if timeblock_1_index <= timeblock_2_index:
-        return connected[timeblock_1][timeblock_2]
-    return connected[timeblock_2][timeblock_1]
+                    for k in range(i + 1, j):
+                        if connected[i][k] and connected[k][j]:
+                            connected[i][j] = True
+                            break
+                    if connected[i][j] is None:
+                        connected[i][j] = False
+        # Final result:
+        timeblock_1_index = all_timeblocks.index(timeblock_1)
+        timeblock_2_index = all_timeblocks.index(timeblock_2)
+        if timeblock_1_index <= timeblock_2_index:
+            return connected[timeblock_1_index][timeblock_2_index]
+        return connected[timeblock_2_index][timeblock_1_index]
--- app/utilities/weekly_timeblocks.py
+++ app/utilities/weekly_timeblocks.py
@@ -1,5 +1,5 @@
 from app.models.weekly_timeblock import WeeklyTimeblock
-from app.utilities.single_timeblocks import are_timeblocks_connected, SingleTimeblock
+from app.utilities.single_timeblocks import SingleTimeblock
 from app.utilities.weekdays import map_int_weekday_to_enum_weekday
 from datetime import datetime
 
@@ -35,11 +35,11 @@ def are_start_time_and_end_time_inside_connected_timeblocks(
         for weekly_timeblock in weekly_timeblocks
         if does_weekly_timeblock_contain_date_time(weekly_timeblock, end_time)
     ]
-    all_timeblocks = [SingleTimeblock(wt) for wt in weekly_timeblocks]
+    all_timeblocks = [SingleTimeblock.from_weekly_timeblock(wt) for wt in weekly_timeblocks]
     for start_weekly_timeblock in weekly_timeblocks_that_contain_start_time:
-        start_timeblock = SingleTimeblock(start_weekly_timeblock)
+        start_timeblock = SingleTimeblock.from_weekly_timeblock(start_weekly_timeblock)
         for end_weekly_timeblock in weekly_timeblocks_that_contain_end_time:
-            end_timeblock = SingleTimeblock(end_weekly_timeblock)
-            if are_timeblocks_connected(start_timeblock, end_timeblock, all_timeblocks):
+            end_timeblock = SingleTimeblock.from_weekly_timeblock(end_weekly_timeblock)
+            if SingleTimeblock.are_timeblocks_connected(start_timeblock, end_timeblock, all_timeblocks):
                 return True
     return False
